c85b0b99bbd3dc992c66337c8653cb0112a37dc5
Bakel Bakel <begededum4bakel@gmail.com>
2025-05-13 02:10:11 +0200

    test 2

--- test2.py
+++ test2.py
@@ -7,11 +7,11 @@ RELAY_PIN = 2
 # Setup
 relay = OutputDevice(RELAY_PIN, active_high=False, initial_value=True)  # Active LOW triggers relay
 
-# Turn on the relay (solenoid ON)
+# Turn on the relay (solenoid ON - Unlock)
 relay.on()  # Relay ON (solenoid unlocked)
 print("Solenoid is ON - Unlocked")
 sleep(5)  # Keep it ON for 5 seconds
 
-# Turn off the relay (solenoid OFF)
+# Turn off the relay (solenoid OFF - Lock)
 relay.off()  # Relay OFF (solenoid locked)
 print("Solenoid is OFF - Locked")
